f38391e22677f23d79ffa297ff68406af570ebfa
Jose Jordan <mundodigital.pro@gmail.com>
2023-07-05 11:21:40 +0200

    implemented image on detail screen

--- app/src/main/java/com/josejordan/rickmortyapp/ui/detail/components/CharacterDetailScreen.kt
+++ app/src/main/java/com/josejordan/rickmortyapp/ui/detail/components/CharacterDetailScreen.kt
@@ -1,24 +1,22 @@
 package com.josejordan.rickmortyapp.ui.detail.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.coil.rememberCoilPainter
 import com.josejordan.rickmortyapp.viewmodel.CharacterDetailViewModel
-import dagger.hilt.android.internal.lifecycle.HiltViewModelFactory
 
-@Composable
+/*@Composable
 fun CharacterDetailScreen(characterId: Int) {
-    //val viewModel: CharacterDetailViewModel = viewModel()
-    //val viewModel: CharacterDetailViewModel = viewModel(factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry))
     val viewModel: CharacterDetailViewModel = hiltViewModel()
 
     LaunchedEffect(characterId) {
@@ -41,4 +39,37 @@ fun CharacterDetailScreen(characterId: Int) {
     } else {
         Text(text = "Loading...")
     }
+}*/
+
+@Composable
+fun CharacterDetailScreen(characterId: Int) {
+    val viewModel: CharacterDetailViewModel = hiltViewModel()
+
+    LaunchedEffect(characterId) {
+        viewModel.fetchCharacterDetail(characterId)
+    }
+
+    val character = viewModel.character.observeAsState(initial = null).value
+
+    // Check if the character data is loaded
+    if (character != null) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // Show character image
+            Image(
+                painter = rememberCoilPainter(request = character.image, fadeIn = true),
+                contentDescription = "Character Image",
+                modifier = Modifier.size(200.dp)
+            )
+            Text(text = character.name)
+            Text(text = character.species)
+            // Add more fields as needed
+        }
+    } else {
+        Text(text = "Loading...")
+    }
 }
+
