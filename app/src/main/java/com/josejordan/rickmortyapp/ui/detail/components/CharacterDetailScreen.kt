a7664e801cb4a06746838131368ba3e9cb68fdfc
Jose Jordan <mundodigital.pro@gmail.com>
2023-07-05 12:18:38 +0200

    better images on list and detail

--- app/src/main/java/com/josejordan/rickmortyapp/ui/detail/components/CharacterDetailScreen.kt
+++ app/src/main/java/com/josejordan/rickmortyapp/ui/detail/components/CharacterDetailScreen.kt
@@ -2,14 +2,20 @@ package com.josejordan.rickmortyapp.ui.detail.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.coil.rememberCoilPainter
@@ -41,7 +47,7 @@ fun CharacterDetailScreen(characterId: Int) {
     }
 }*/
 
-@Composable
+/*@Composable
 fun CharacterDetailScreen(characterId: Int) {
     val viewModel: CharacterDetailViewModel = hiltViewModel()
 
@@ -71,5 +77,57 @@ fun CharacterDetailScreen(characterId: Int) {
     } else {
         Text(text = "Loading...")
     }
+}*/
+
+@Composable
+fun CharacterDetailScreen(characterId: Int) {
+    val viewModel: CharacterDetailViewModel = hiltViewModel()
+
+    LaunchedEffect(characterId) {
+        viewModel.fetchCharacterDetail(characterId)
+    }
+
+    val character = viewModel.character.observeAsState(initial = null).value
+
+    // Check if the character data is loaded
+    if (character != null) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Show character image
+            Image(
+                painter = rememberCoilPainter(request = character.image, fadeIn = true),
+                contentDescription = "Character Image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(15.dp)) // This will make the image corners rounded
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                //elevation = 8.dp,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = "Name: ${character.name}")
+                    Text(text = "Species: ${character.species}")
+                    Text(text = "Gender: ${character.gender}")
+                    Text(text = "Location: ${character.location.name}")
+                    // Add more fields as needed
+                }
+            }
+        }
+
+    } else {
+        Text(text = "Loading...")
+    }
 }
+
 
--- app/src/main/java/com/josejordan/rickmortyapp/ui/home/components/CharacterListItem.kt
+++ app/src/main/java/com/josejordan/rickmortyapp/ui/home/components/CharacterListItem.kt
@@ -2,17 +2,87 @@ package com.josejordan.rickmortyapp.ui.home.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
 import com.josejordan.rickmortyapp.data.Character
 
+@Composable
+fun CharacterListItem(character: Character, onClick: (Character) -> Unit) {
+    Card(
+        shape = RoundedCornerShape(15.dp),
+        //elevation = 10.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { onClick(character) }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = rememberCoilPainter(request = character.image),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(15.dp)),
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = character.name)
+        }
+    }
+}
+
+
+/*
+@Composable
+fun CharacterListItem(character: Character, onClick: (Character) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { onClick(character) }
+    ) {
+        Card(
+            shape = RoundedCornerShape(15.dp),
+            //elevation = 10.dp,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = rememberCoilPainter(request = character.image),
+                contentDescription = null,
+                //modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp), // Set the height here
+                contentScale = ContentScale.Fit
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = character.name)
+    }
+}
+
+*/
+
+/*
 @Composable
 fun CharacterListItem(character: Character, onClick: (Character) -> Unit) {
     Column(
@@ -30,3 +100,4 @@ fun CharacterListItem(character: Character, onClick: (Character) -> Unit) {
         Text(text = character.name)
     }
 }
+*/
