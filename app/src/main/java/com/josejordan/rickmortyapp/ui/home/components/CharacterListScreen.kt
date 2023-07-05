a55bd7a1959cba47eabc1e2eef3506bf032c2869
Jose Jordan <mundodigital.pro@gmail.com>
2023-07-05 12:53:15 +0200

    better images on list and detail

--- app/src/main/java/com/josejordan/rickmortyapp/ui/home/components/CharacterListScreen.kt
+++ app/src/main/java/com/josejordan/rickmortyapp/ui/home/components/CharacterListScreen.kt
@@ -1,20 +1,90 @@
 package com.josejordan.rickmortyapp.ui.home.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.josejordan.rickmortyapp.viewmodel.CharacterDetailViewModel
 import com.josejordan.rickmortyapp.viewmodel.CharacterListViewModel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun CharacterListScreen(navController: NavController, viewModel: CharacterListViewModel = hiltViewModel()) {
+    val filteredCharacters = viewModel.filteredCharacters.observeAsState(initial = emptyList()).value
+
+    // Begin with a Column layout so we can put things vertically one after another.
+    Column {
+        // TextField for the search input at the top
+        OutlinedTextField(
+            value = viewModel.searchQuery.value ?: "",
+            onValueChange = { viewModel.setSearchQuery(it) },
+            label = { Text("Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        // LazyColumn for the list of characters below
+        LazyColumn {
+            items(filteredCharacters) { character ->
+                CharacterListItem(character) {
+                    navController.navigate("character_detail/${it.id}")
+                }
+            }
+        }
+    }
+}
+
+
+
+/*@Composable
 fun CharacterListScreen(navController: NavController) {
-    //val viewModel: CharacterListViewModel = viewModel()
     val viewModel: CharacterListViewModel = hiltViewModel()
     val characters = viewModel.characters.observeAsState(initial = emptyList()).value
+
+    var searchTerm by remember { mutableStateOf("") }
+
+    Column {
+        OutlinedTextField(
+            value = searchTerm,
+            onValueChange = { newSearchTerm ->
+                searchTerm = newSearchTerm
+                viewModel.fetchAllCharacters(searchTerm)
+            },
+            label = { Text("Search Characters") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        LazyColumn {
+            items(characters) { character ->
+                CharacterListItem(character) {
+                    navController.navigate("character_detail/${it.id}")
+                }
+            }
+        }
+    }
+}*/
+
+
+/*
+@Composable
+fun CharacterListScreen(navController: NavController) {
+    val viewModel: CharacterListViewModel = hiltViewModel()
+    val characters = viewModel.characters.observeAsState(initial = emptyList()).value
+    var searchTerm by remember { mutableStateOf("") }
+
 
     LazyColumn {
         items(characters) { character ->
@@ -24,3 +94,4 @@ fun CharacterListScreen(navController: NavController) {
         }
     }
 }
+*/
--- app/src/main/java/com/josejordan/rickmortyapp/viewmodel/CharacterListViewModel.kt
+++ app/src/main/java/com/josejordan/rickmortyapp/viewmodel/CharacterListViewModel.kt
@@ -1,6 +1,7 @@
 package com.josejordan.rickmortyapp.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -17,12 +18,100 @@ class CharacterListViewModel @Inject constructor(
 
     val characters = MutableLiveData<List<Character>>()
     val error = MutableLiveData<String>()
+    val searchQuery = MutableLiveData<String>("")
+
+    // LiveData to hold filtered characters
+    val filteredCharacters = MediatorLiveData<List<Character>>()
+
+    init {
+        fetchAllCharacters()
+
+        // Observe changes in characters and searchQuery
+        filteredCharacters.addSource(characters) {
+            filteredCharacters.value = filterCharacters(it, searchQuery.value ?: "")
+        }
+        filteredCharacters.addSource(searchQuery) {
+            filteredCharacters.value = filterCharacters(characters.value ?: emptyList(), it)
+        }
+    }
+
+    private fun fetchAllCharacters() {
+        viewModelScope.launch {
+            try {
+                val response = characterRepository.getAllCharacters()
+                if (response.isSuccessful) {
+                    response.body()?.results?.let { characters.postValue(it) }
+                    Log.d("CharacterListViewModel", "Characters fetched successfully.")
+                } else {
+                    error.postValue("Error: ${response.code()}")
+                    Log.e("CharacterListViewModel", "Error fetching characters: ${response.code()}")
+                }
+            } catch (e: Exception) {
+                error.postValue(e.message)
+                Log.e("CharacterListViewModel", "Exception fetching characters: ${e.message}")
+            }
+        }
+    }
+
+    // Function to filter characters based on the search query
+    private fun filterCharacters(characters: List<Character>, query: String): List<Character> {
+        return if (query.isEmpty()) {
+            characters
+        } else {
+            characters.filter { character ->
+                character.name.contains(query, ignoreCase = true)
+            }
+        }
+    }
+
+    fun setSearchQuery(query: String) {
+        searchQuery.value = query
+    }
+}
+
+
+
+/*@HiltViewModel
+class CharacterListViewModel @Inject constructor(
+    private val characterRepository: CharacterRepository
+) : ViewModel() {
+
+    val characters = MutableLiveData<List<Character>>()
+    val error = MutableLiveData<String>()
+
+    fun fetchAllCharacters(searchTerm: String = "") {
+        viewModelScope.launch {
+            try {
+                val response = characterRepository.getAllCharacters(searchTerm)
+                if (response.isSuccessful) {
+                    response.body()?.results?.let { characters.postValue(it) }
+                    Log.d("CharacterListViewModel", "Characters fetched successfully.")
+                } else {
+                    error.postValue("Error: ${response.code()}")
+                    Log.e("CharacterListViewModel", "Error fetching characters: ${response.code()}")
+                }
+            } catch (e: Exception) {
+                error.postValue(e.message)
+                Log.e("CharacterListViewModel", "Exception fetching characters: ${e.message}")
+            }
+        }
+    }
+}*/
+
+
+/*@HiltViewModel
+class CharacterListViewModel @Inject constructor(
+    private val characterRepository: CharacterRepository
+) : ViewModel() {
+
+    val characters = MutableLiveData<List<Character>>()
+    val error = MutableLiveData<String>()
 
     init {
         fetchAllCharacters()
     }
 
-private fun fetchAllCharacters() {
+fun fetchAllCharacters() {
     viewModelScope.launch {
         try {
             val response = characterRepository.getAllCharacters()
@@ -40,4 +129,4 @@ private fun fetchAllCharacters() {
     }
 }
 
-}
+}*/
